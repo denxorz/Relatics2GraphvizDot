02840949bb2eab1d80772591dc8f5c538381eadd
Dennis Geldhof <dennis@geldhof.eu>
2025-07-02 21:12:22 +0200

    Use other dot library

--- Relatics2GraphvizDot/Program.cs
+++ Relatics2GraphvizDot/Program.cs
@@ -1,28 +1,26 @@
-﻿using System.Xml.Serialization;
-using GiGraph.Dot.Entities.Graphs;
-using Relatics2GraphvizDot.InputXml;
-using GiGraph.Dot.Entities.Graphs;
-using GiGraph.Dot.Extensions;
-using GiGraph.Dot.Types.Nodes.Style;
-using System.Drawing;
+﻿using System.Drawing;
+using System.Xml.Serialization;
+using DotNetGraph.Compilation;
+using DotNetGraph.Core;
+using DotNetGraph.Extensions;
 using GraphVizNet;
-using GiGraph.Dot.Types.Nodes;
+using Relatics2GraphvizDot.InputXml;
 
 XmlSerializer serializer = new XmlSerializer(typeof(elements));
 using FileStream fileStream = new FileStream("test 2025-07-02 18_26 UTC+2.xml", FileMode.Open);
-elements result = (elements?)serializer.Deserialize(fileStream) ?? new();
+elements relaticsXml = (elements?)serializer.Deserialize(fileStream) ?? new();
 
-var factoryNamesById = result
+var factoryNamesById = relaticsXml
     .Factory
     .Element
     .ToDictionary(f => f.id, f => new Factory(f.id, f.name));
 
-var itemNamesById = result
+var itemNamesById = relaticsXml
     .Item
     .Element
     .ToDictionary(f => f.id, f => new Item(f.id, f.name));
 
-var transportNamesById = result
+var transportNamesById = relaticsXml
     .FactoryImport
     .Element
     .ToDictionary(f => f.id, f => new Transport(
@@ -33,40 +31,44 @@ var transportNamesById = result
         factoryNamesById[f.Shipfrom.Factory.id],
         factoryNamesById[f.providesitemsto.Factory.id]));
 
-Console.WriteLine(result.Factory);
+var graph = new DotGraph().WithIdentifier("MyDirectedGraph").Directed();
 
-
-
-var graph = new DotGraph(directed: true);
-
-foreach(var f in factoryNamesById.Values)
+foreach (var f in factoryNamesById.Values)
 {
-    graph.Nodes.Add(f.Id, node =>
-    {
-        node.Label = f.Name;
-        node.Shape = DotNodeShape.Rectangle;
-    });
+    graph.Add(
+        new DotNode()
+            .WithIdentifier(f.Id)
+            .WithShape(DotNodeShape.Rectangle)
+            .WithLabel(f.Name));
 }
 
 foreach (var t in transportNamesById.Values)
 {
-    graph.Edges.Add(t.From.Id, t.To.Id, edge =>
-    {
-        edge.Label = $"{t.AmountPerMinute:N0}";
-        edge.Style.LineColor = t.Color;
-    });
+    graph.Add(
+        new DotEdge()
+            .From(t.From.Id)
+            .To(t.To.Id)
+            .WithLabel($"{t.AmountPerMinute:N0}")
+            .WithArrowHead(DotEdgeArrowType.Normal)
+            .WithColor(t.Color)
+            .WithStyle(DotEdgeStyle.Dashed)
+            .WithPenWidth(1.5));
 }
 
+await using var writer = new StringWriter();
+await graph.CompileAsync(new CompilationContext(writer, new CompilationOptions()));
+var dot = writer.GetStringBuilder().ToString();
+
 var graphViz = new GraphViz();
-graphViz.LayoutAndRenderDotGraph(graph.ToDot(), "test.png", "png");
+graphViz.LayoutAndRenderDotGraph(dot, "..\\test.png", "png");
 
 record Factory(string Id, string Name);
 record Item(string Id, string Name);
 record Transport(string Id, Item Item, decimal AmountPerMinute, string Method, Factory From, Factory To)
 {
-    public Color Color => Item.Name switch
+    public DotColor Color => Item.Name switch
     {
-        "Rubber" => Color.DarkGray,
-        _ => Color.Aqua,
+        "Rubber" => DotColor.DarkGray,
+        _ => DotColor.Aqua,
     };
 };
